59e6f06e5125f001603246196f475ea7386639db
Yogesh Kanwade <90169068+yogeshkanwade21@users.noreply.github.com>
2024-06-29 21:30:19 +0530

    Toggle on/off

--- Counter/src/App.jsx
+++ Counter/src/App.jsx
@@ -2,44 +2,62 @@ import { useRef } from "react";
 import { useState } from "react";
 
 function App() {
+  const [flag, setFlag] = useState(false);
 
-  const [count, setCount] = useState(0);
-  const [isIncrementing, setIsIncrementing] = useState(false);
-  const incrementInterval = useRef(null);
-
-  const toggleIncrement = () => {
-    if (isIncrementing) {
-      clearInterval(incrementInterval.current);
-      setCount(0);
-    }
-    else {
-      incrementInterval.current = setInterval(() => {
-        setCount(count => count + 2);
-      }, 1000);
-    }
-    setIsIncrementing(!isIncrementing);
-  };
-
-  const decrementCount = () => {
-    setCount(count - 2);
+  // event handler
+  const handleToggle = () => {
+    setFlag(!flag);
   }
 
   return (
     <>
-      <h1>Counter is {count}</h1>
+      <h1> Switch is {flag? "On" : "Off"} </h1>
       <button
-      onClick={toggleIncrement}
+      onClick={handleToggle} 
       >
-        {!isIncrementing ? "Increment" : "Stop Increment"}
+        Toggle
       </button>
-      <button
-      onClick={decrementCount}
-      >
-        Decrement
-      </button>
-    </>
 
+    </>
   )
+
+  // const [count, setCount] = useState(0);
+  // const [isIncrementing, setIsIncrementing] = useState(false);
+  // const incrementInterval = useRef(null);
+
+  // const toggleIncrement = () => {
+  //   if (isIncrementing) {
+  //     clearInterval(incrementInterval.current);
+  //     setCount(0);
+  //   }
+  //   else {
+  //     incrementInterval.current = setInterval(() => {
+  //       setCount(count => count + 2);
+  //     }, 1000);
+  //   }
+  //   setIsIncrementing(!isIncrementing);
+  // };
+
+  // const decrementCount = () => {
+  //   setCount(count - 2);
+  // }
+
+  // return (
+  //   <>
+  //     <h1>Counter is {count}</h1>
+  //     <button
+  //     onClick={toggleIncrement}
+  //     >
+  //       {!isIncrementing ? "Increment" : "Stop Increment"}
+  //     </button>
+  //     <button
+  //     onClick={decrementCount}
+  //     >
+  //       Decrement
+  //     </button>
+  //   </>
+
+  // )
 }
 
 export default App;
