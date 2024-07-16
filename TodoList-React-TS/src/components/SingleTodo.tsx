632474c642f9ba5b400e44ba7f4951ec56e0e042
yogesh_kanwade <yogesh.kanwade@bee-logical.com>
2024-07-16 13:00:27 +0530

    feat: edit Todo

--- TodoList-React-TS/src/components/SingleTodo.tsx
+++ TodoList-React-TS/src/components/SingleTodo.tsx
@@ -1,6 +1,7 @@
 import { MdEdit, MdDelete } from "react-icons/md";
 import { FaCheckCircle } from "react-icons/fa";
 import { Todo } from "../model";
+import { useState, useRef, useEffect } from "react";
 
 type SingleTodoProps = {
     todo: Todo;
@@ -10,17 +11,37 @@ type SingleTodoProps = {
 
 const SingleTodo = ({todo, todos, setTodos}: SingleTodoProps) => {
 
-  const handleDone = (id: number) => {
-    setTodos(todos.map((todo) =>
-        todo.id ===id ? {...todo, isDone: !todo.isDone} : todo));
-  }
+    const [edit, setEdit] = useState<boolean>(false);
+    const [editTodo, setEditTodo] = useState<string>(todo.todo);
 
-  const handleDelete = (id: number) => {
-      setTodos(todos.filter((todo) => todo.id !== id));
-  }
+    const handleEdit = ( e: React.FormEvent, id: number) => {
+        e.preventDefault();
+        setTodos(todos.map((todo) => 
+            todo.id === id ? {...todo, todo: editTodo} : todo
+        ));
+        setEdit(false);
+    }
+
+    const inputRef = useRef<HTMLInputElement>(null);
+
+    const handleDone = (id: number) => {
+        setTodos(todos.map((todo) =>
+            todo.id ===id ? {...todo, isDone: !todo.isDone} : todo));
+    }
+
+    const handleDelete = (id: number) => {
+        setTodos(todos.filter((todo) => todo.id !== id));
+    }
+
+    useEffect(() => {
+        inputRef.current?.focus();
+    }, [edit])
+
   return (
-    <form className="todos__single">
-        {
+    <form className="todos__single" onSubmit={(e) => handleEdit(e, todo.id)}>
+        {edit ? (
+            <input ref={inputRef} value={editTodo} onChange={(e) => setEditTodo(e.target.value)} className="todos_single--text"/>
+        ) :
             todo.isDone ? (
                 <span className="todos__single--text"><s>{todo.todo}</s></span>
             ) : (
@@ -28,7 +49,11 @@ const SingleTodo = ({todo, todos, setTodos}: SingleTodoProps) => {
                 )
         }
         <div>
-            <span className="icon">
+            <span className="icon" onClick={() => {
+                if(!edit && !todo.isDone) {
+                    setEdit(!edit);
+                }   
+            }}>
                 <MdEdit />
             </span>
             <span className="icon" onClick={() => handleDone(todo.id)}>
