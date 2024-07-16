dc9497f3cdf960b77f0e0511a6ac0bc8c6a48f36
yogesh_kanwade <yogesh.kanwade@bee-logical.com>
2024-07-16 16:11:41 +0530

    feat: sort todos (alphabetically and newest first)

--- TodoList-React-TS/src/components/TodoList.tsx
+++ TodoList-React-TS/src/components/TodoList.tsx
@@ -11,12 +11,41 @@ interface TodoListProps {
 const TodoList = ({todos,  setTodos}: TodoListProps) => {
 
     const [filter, setFilter] = useState<string>("all");
+    const [sort, setSort] = useState<string>("none");
+
+    const handleSortChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setSort(event.target.value);
+    }
 
     const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
         setFilter(event.target.value);
     }
+
+    const filteredTodos = todos.filter((todo) => {
+        if(filter === "completed") {
+            return todo.isDone;
+        }
+        if(filter === "pending") {
+            return !todo.isDone;
+        }
+        return true;
+    });
+
+    const getSortedTodos = ( todos: Todo[] ) => {
+        if(sort === "alphabetically") {
+            return todos.slice().sort((a, b) => a.todo.localeCompare(b.todo, 'en', { sensitivity: 'base' }));
+        }
+        if(sort === "newest-first") {
+            return todos.slice().sort((a, b) => b.id - a.id);
+        }
+        return todos;
+    }
+
+    const sortedTodos = getSortedTodos(filteredTodos);
+
     return (
         <>
+        <div className="options">
             <div className="filter">
                 <label className="filter_label" htmlFor="select-filter">Filter:</label>
                     <select id="select-filter" value={filter} onChange={handleFilterChange}>
@@ -24,19 +53,19 @@ const TodoList = ({todos,  setTodos}: TodoListProps) => {
                         <option value="completed">Completed</option>
                         <option value="pending">Pending</option>
                     </select>
-        
             </div>
+            <div className="sort">
+                <label className="sort_label" htmlFor="select-sort">Sort:</label>
+                    <select id="select-sort" value={sort} onChange={handleSortChange}>
+                        <option value="none">None</option>
+                        <option value="alphabetically">Alphabetically</option>
+                        <option value="newest-first">Newest First</option>
+                    </select>
+            </div>
+        </div>
+
             <div className="todos">
-                {todos.filter((todo) => {
-                    if(filter === "completed") {
-                        return todo.isDone;
-                    }
-                    if(filter === "pending") {
-                        return !todo.isDone;
-                    }
-                    return true;
-                })
-                .map((todo) => (
+                {sortedTodos.map((todo) => (
                     <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
                 ))}
             </div>
