da3235cd1f2f57e5e340385f0ba6aa185a2d9ad7
yogesh_kanwade <yogesh.kanwade@bee-logical.com>
2024-07-16 14:40:39 +0530

    feat: filter completed and pending todos

--- TodoList-React-TS/src/components/TodoList.tsx
+++ TodoList-React-TS/src/components/TodoList.tsx
@@ -1,6 +1,7 @@
 import "./styles.css";
 import { Todo } from "../model";
 import SingleTodo from "./SingleTodo";
+import { useState } from "react";
 
 interface TodoListProps {
     todos: Todo[];
@@ -8,13 +9,39 @@ interface TodoListProps {
 }
 
 const TodoList = ({todos,  setTodos}: TodoListProps) => {
-  return (
-    <div className="todos">
-        {todos.map((todo) => (
-            <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
-        ))}
-    </div>
-  )
+
+    const [filter, setFilter] = useState<string>("all");
+
+    const handleFilterChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setFilter(event.target.value);
+    }
+    return (
+        <>
+            <div className="filter">
+                <label className="filter_label" htmlFor="select-filter">Filter:</label>
+                    <select id="select-filter" value={filter} onChange={handleFilterChange}>
+                        <option value="all">All</option>
+                        <option value="completed">Completed</option>
+                        <option value="pending">Pending</option>
+                    </select>
+        
+            </div>
+            <div className="todos">
+                {todos.filter((todo) => {
+                    if(filter === "completed") {
+                        return todo.isDone;
+                    }
+                    if(filter === "pending") {
+                        return !todo.isDone;
+                    }
+                    return true;
+                })
+                .map((todo) => (
+                    <SingleTodo todo={todo} key={todo.id} todos={todos} setTodos={setTodos}/>
+                ))}
+            </div>
+        </>
+    )
 }
 
 export default TodoList;
