9bc33e4a588d87da1b797c4367ca32d1b6d115e2
yogeshkanwade21 <yogeshkanwade2015@gmail.com>
2024-07-18 16:01:01 +0530

    add more reducer functions

--- redux-toolkit/redux-1/src/features/counter/Counter.jsx
+++ redux-toolkit/redux-1/src/features/counter/Counter.jsx
@@ -1,15 +1,25 @@
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import { increment, decrement, incrementByAmount, reset } from './counterSlice';
+import { useState } from 'react';
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.count);
     const dispatch = useDispatch();
+
+    const [incrementAmount, setIncrementAmount] = useState(5);
+
+    const resetAll = () => {
+        setIncrementAmount(0);
+        dispatch(reset());
+    }
 
     return (
         <section>
             <h2>{count}</h2>
             <button onClick={() => dispatch(increment())}>increment</button>
             <button onClick={() => dispatch(decrement())}>decrement</button>
+            <button onClick={() => dispatch(incrementByAmount(incrementAmount))}>increment by 5</button>
+            <button onClick={() => resetAll()}>Reset</button>
         </section>
     )
 }
