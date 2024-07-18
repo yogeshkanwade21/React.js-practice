99bb13187c965612799751f3f4ae09f125db3e8f
yogeshkanwade21 <yogeshkanwade2015@gmail.com>
2024-07-18 17:42:55 +0530

    separation of concerns between the action's payload creation and the state mutation logic

--- redux-toolkit/redux-bulletin-board/src/features/posts/AddPostForm.jsx
+++ redux-toolkit/redux-bulletin-board/src/features/posts/AddPostForm.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { nanoid } from '@reduxjs/toolkit';
 
 import { postAdded } from './postsSlice';
 
@@ -16,11 +15,7 @@ const AddPostForm = () => {
     const onSavePostClicked = () => {
         if (title && content) {
             dispatch(
-                postAdded({
-                    id: nanoid(),
-                    title,
-                    content
-                })
+                postAdded(title, content)
             )
 
             setTitle('');
